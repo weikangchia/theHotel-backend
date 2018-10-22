f62317b0ed73b4a6b46ea6303259139de8694cc1
Wei Kang <weikangchia@gmail.com>
2018-10-22 10:30:14 +0800

    add hotel description

--- endpoints/hotel/readAll.js
+++ endpoints/hotel/readAll.js
@@ -13,6 +13,7 @@ module.exports.handler = async (event) => {
       hourly_rate: 40,
       minute_rate: 0.5,
       image_url: 'https://res.cloudinary.com/geboto/image/upload/v1540112510/thehotel/hotel1.jpg',
+      description: 'The J&J Hotel is an ideal base to explore Singapore from, as its neighborhood offers many popular dining and entertainment choices.\n\nAll air-conditioned rooms are tastefully decorated with Baroque furnishings, floor-to-ceiling windows, a view of the city or river, and tea and coffee-making facilities.',
     },
     {
       id: 2,
@@ -21,6 +22,7 @@ module.exports.handler = async (event) => {
       hourly_rate: 30,
       minute_rate: 0.5,
       image_url: 'https://res.cloudinary.com/geboto/image/upload/v1540112510/thehotel/hotel2.jpg',
+      description: 'The Beta hotel’s rooms combine high-style and modern comforts with wall murals, LCD TV, iPod dock, coffeemaker, and free Wi-Fi. Bathrooms have marble floors, tubs and designer bathing products.\n\nAfter a short walk to a nearby MRT station, guests can easily access the island\'s abundant entertainment options: The National Museum, Clarke Quay, Singapore Zoo, The Marine Life Park, to name a few!',
     },
   ]);
 
